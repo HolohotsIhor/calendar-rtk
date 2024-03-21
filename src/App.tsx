71f16d1003cad53b2280c39f8976e3c7aeab2f4e
airSlateHolohots <ihor.holohots@airslate.com>
2024-03-21 21:19:16 +0200

    51 : 40

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React, {FC} from 'react';
+import React, { FC, useEffect } from 'react';
 import { AppRouter } from './components/appRouter/AppRouter';
 import { Navbar } from './components/navbar/Navbar';
 import { Layout } from 'antd';
 import './App.styl';
+import { useActions } from './hooks/useActions';
+import { IUser } from './models/IUser';
 
 export const App:FC = () => {
+  const { setUser, setIsAuth } = useActions()
+  useEffect(() => {
+    if (localStorage.getItem('auth')) {
+        // For real cases need to check tocken from localStorage to API
+        setUser({ username: localStorage.getItem('username' || '')} as IUser)
+        setIsAuth(true)
+    }
+  }, []);
+
   return (
     <Layout>
         <Navbar />
