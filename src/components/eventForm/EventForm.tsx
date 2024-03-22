dbffc9fa6c97e93a3fe654d2657423c0c7b1556f
airSlateHolohots <ihor.holohots@airslate.com>
2024-03-22 15:49:32 +0200

    Add event form

--- src/components/eventForm/EventForm.tsx
+++ src/components/eventForm/EventForm.tsx
@@ -1,8 +1,13 @@
 import React, { FC } from 'react';
-import { Form, Input } from 'antd';
+import { Button, DatePicker, DatePickerProps, Form, Input, Row } from 'antd';
 import { inputRules } from '../../utils/input-rules';
+import './EventForm.styl'
 
 export const EventForm: FC  = () => {
+    const onChange: DatePickerProps['onChange'] = (date, dateString) => {
+        console.log(date, dateString);
+    };
+
     return (
         <Form>
             <Form.Item
@@ -12,6 +17,18 @@ export const EventForm: FC  = () => {
             >
                 <Input />
             </Form.Item>
+            <Form.Item
+                label='Event date'
+                name='date'
+                rules={[inputRules.required()]}
+            >
+                <DatePicker onChange={onChange} />
+            </Form.Item>
+            <Row className='event-form__action'>
+                <Button type='primary' htmlType='submit'>
+                    Submit
+                </Button>
+            </Row>
         </Form>
     );
 }
